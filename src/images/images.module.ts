a2378e45f37b0d80e3676d7b660bdf5d167b3a51
Onur <onurkapcik@gmail.com>
2023-06-23 01:50:35 +0300

    Revert static files.

--- src/images/images.module.ts
+++ src/images/images.module.ts
@@ -1,19 +1,12 @@
 import { Module } from '@nestjs/common';
 import { APP_INTERCEPTOR } from '@nestjs/core';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
 
 import { ImagesController } from './api/images.controller';
 import { GlobalExceptionInterceptor } from '../shared/interceptors/global-exception.interceptor';
 import { ImagesService } from './services/images.service';
 
 @Module({
-  imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'swagger-static'),
-      serveRoot: process.env.NODE_ENV === 'development' ? '/' : '/swagger',
-    }),
-  ],
+  imports: [],
   controllers: [ImagesController],
   providers: [
     ImagesService,
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
-import { createWriteStream } from 'fs';
+import * as path from 'path';
+import { writeFileSync } from 'fs';
 
 import { ImagesModule } from './images/images.module';
-import { get } from 'http';
 
 async function bootstrap() {
   const app = await NestFactory.create(ImagesModule);
@@ -17,51 +17,10 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
+  const outputPath = path.resolve(process.cwd(), 'swagger.json');
+  writeFileSync(outputPath, JSON.stringify(document), { encoding: 'utf8' });
 
   SwaggerModule.setup('/swagger', app, document);
-  if (process.env.NODE_ENV === 'development') {
-    get(
-      `https://image-reshaper.vercel.app/swagger/swagger-ui-bundle.js`,
-      function (response) {
-        response.pipe(createWriteStream('swagger-static/swagger-ui-bundle.js'));
-        console.log(
-          `Swagger UI bundle file written to: '/swagger-static/swagger-ui-bundle.js'`,
-        );
-      },
-    );
-
-    get(
-      `https://image-reshaper.vercel.app/swagger/swagger-ui-init.js`,
-      function (response) {
-        response.pipe(createWriteStream('swagger-static/swagger-ui-init.js'));
-        console.log(
-          `Swagger UI init file written to: '/swagger-static/swagger-ui-init.js'`,
-        );
-      },
-    );
-
-    get(
-      `https://image-reshaper.vercel.app/swagger/swagger-ui-standalone-preset.js`,
-      function (response) {
-        response.pipe(
-          createWriteStream('swagger-static/swagger-ui-standalone-preset.js'),
-        );
-        console.log(
-          `Swagger UI standalone preset file written to: '/swagger-static/swagger-ui-standalone-preset.js'`,
-        );
-      },
-    );
-
-    get(
-      `https://image-reshaper.vercel.app/swagger/swagger-ui.css`,
-      function (response) {
-        response.pipe(createWriteStream('swagger-static/swagger-ui.css'));
-        console.log(
-          `Swagger UI css file written to: '/swagger-static/swagger-ui.css'`,
-        );
-      },
-    );
-  }
 
   await app.listen(3000);
 }
