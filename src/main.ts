b83b789610deeda7f37cc4c3e7809173598f93f7
Onur <onurkapcik@gmail.com>
2023-06-23 01:35:09 +0300

    Try to generate swagger.json on startup.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
-import { resolve } from 'path';
-import { writeFileSync } from 'fs';
+import * as fs from 'fs';
 
 import { ImagesModule } from './images/images.module';
 
@@ -19,15 +18,10 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
 
   SwaggerModule.setup('/swagger', app, document);
+  const swaggerJson = JSON.stringify(document, null, 2);
+  fs.writeFileSync('./public/swagger.json', swaggerJson);
 
   await app.listen(3000);
-  if (process.env.NODE_ENV === 'development') {
-    const swaggerFolderPath = resolve(process.cwd(), 'swagger-docs');
-    const swaggerJsonPath = resolve(swaggerFolderPath, 'swagger.json');
-    const swaggerJson = JSON.stringify(document, null, 2);
-    writeFileSync(swaggerJsonPath, swaggerJson);
-    console.log(`Swagger JSON file written to: '/swagger-docs/swagger.json'`);
-  }
 }
 
 bootstrap();
