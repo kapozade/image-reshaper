89903fe1819cad53557bfb85cdb36e8c4a9aa297
Onur <onurkapcik@gmail.com>
2023-06-22 22:38:47 +0300

    Implement mappings.

---
+++ src/images/services/commands/reshape-image.command.ts
@@ -0,0 +1,52 @@
+import { ImageTypes } from 'src/images/image-extensions.enum';
+
+export class ReshapeImageCommand {
+  readonly content: string;
+  readonly options: ReshapeImageOptionCommand[];
+
+  constructor(content: string, options: ReshapeImageOptionCommand[]) {
+    this.content = content;
+    this.options = options;
+  }
+}
+
+export class ReshapeImageOptionCommand {
+  readonly extension: ImageTypes;
+  readonly quality: number | null;
+  readonly width: number;
+  readonly height: number;
+
+  constructor(
+    width: number,
+    height: number,
+    quality: number | null,
+    extension: ImageTypes,
+  ) {
+    this.extension = extension;
+    this.quality = quality;
+    this.height = height;
+    this.width = width;
+  }
+}
+
+export class ReshapeImageCommandResult {
+  readonly content: string;
+  readonly width: number;
+  readonly height: number;
+  readonly quality: number | null;
+  readonly extension: ImageTypes;
+
+  constructor(
+    content: string,
+    width: number,
+    height: number,
+    quality: number | null,
+    extension: ImageTypes,
+  ) {
+    this.content = content;
+    this.width = width;
+    this.height = height;
+    this.quality = quality;
+    this.extension = extension;
+  }
+}
