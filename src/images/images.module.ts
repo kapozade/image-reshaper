5e0ae86e6734a9e03cb7bb5b21e243f8fe09d503
Onur <onurkapcik@gmail.com>
2023-06-23 01:20:44 +0300

    Add swagger files.

--- src/images/images.module.ts
+++ src/images/images.module.ts
@@ -1,12 +1,19 @@
 import { Module } from '@nestjs/common';
 import { APP_INTERCEPTOR } from '@nestjs/core';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 import { ImagesController } from './api/images.controller';
 import { GlobalExceptionInterceptor } from '../shared/interceptors/global-exception.interceptor';
 import { ImagesService } from './services/images.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'swagger-docs'),
+      serveRoot: process.env.NODE_ENV === 'development' ? '/' : '/swagger',
+    }),
+  ],
   controllers: [ImagesController],
   providers: [
     ImagesService,
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { resolve } from 'path';
+import { writeFileSync } from 'fs';
 
 import { ImagesModule } from './images/images.module';
 
@@ -16,9 +18,16 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
 
-  SwaggerModule.setup('/', app, document);
+  SwaggerModule.setup('/swagger', app, document);
 
   await app.listen(3000);
+  if (process.env.NODE_ENV === 'development') {
+    const swaggerFolderPath = resolve(process.cwd(), 'swagger-docs');
+    const swaggerJsonPath = resolve(swaggerFolderPath, 'swagger.json');
+    const swaggerJson = JSON.stringify(document, null, 2);
+    writeFileSync(swaggerJsonPath, swaggerJson);
+    console.log(`Swagger JSON file written to: '/swagger-docs/swagger.json'`);
+  }
 }
 
 bootstrap();
